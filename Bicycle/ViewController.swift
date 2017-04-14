824fd617e2ddbefffdbd81a7dbee4d8b58fe3dc0
Kyra Catabay <catabay.kyra@gmail.com>
2017-04-14 13:08:56 -0400

    My first commit

--- Bicycle/ViewController.swift
+++ Bicycle/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount=0
+    
+    
+    
+    @IBAction func STARTbutton(_ sender: Any) {
+        
+        tapCount=tapCount + 1
+        print(tapCount)
+        
+        
+        if tapCount == 1 {
+        theLabel.text = ("One")
+        }
+        
+        if tapCount == 2 {
+            theLabel.text = ("Two")
+        }
+        
+        if tapCount == 3 {
+            theLabel.text = ("Three")
+        }
+        
+        if tapCount == 4 {
+            theLabel.text = ("Four")
+        }
+        
+        if tapCount == 5 {
+            theLabel.text = ("Five")
+        }
+        
+        if tapCount >= 6 {
+            theLabel.text = ("We can count to five :)")
+        }
+    }
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
